20ed8d4ba7bc7153993c7f4ce1bd58fd4d8a51f9
ParthJindal23k <parthjindal23316@gmail.com>
2026-01-15 02:03:37 +0530

    db changed

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,17 @@
-const mongoose = require("mongoose")
-const connectDatabase = async()=>{
-    try {
-        await mongoose.connect(process.env.MONGO_URL)
-        console.log("MongoDB connected")
-    } catch (error) {
-        console.log(error.message)
+const mongoose = require("mongoose");
 
-    }
-}
+const connectDatabase = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 30000, 
+      socketTimeoutMS: 45000,
+    });
+    
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error connecting to MongoDB: ${error.message}`);
+    process.exit(1);
+  }
+};
 
-module.exports = connectDatabase
+module.exports = connectDatabase;
