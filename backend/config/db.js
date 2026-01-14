42b03ec6d71a3f7ce59dc287b5a59a393da92338
ParthJindal23k <parthjindal23316@gmail.com>
2026-01-15 02:05:15 +0530

    rename url

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDatabase = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(process.env.MONGO_URL, {
       serverSelectionTimeoutMS: 30000, 
       socketTimeoutMS: 45000,
     });
